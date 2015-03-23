f00ca25f2a09915cd13f63cd9c9a8d1e620df2d5
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-03-23 16:25:54 -0700

    Cleanup.  Changes to be committed: 	modified:   database.pl

--- database.pl
+++ database.pl
@@ -1,40 +1,5 @@
 
-%%% Server
-
-
-%--------------------------------------------------------------------------------%
-% Commands
-%--------------------------------------------------------------------------------%
-
-% This creates a new user in the database, or updates a user token in the database
-% USERNAME: The user to update
-% ping USERNAME
-% This updates a user token in the database, useful for keeping a user logged in
-% This logs a user out of the system
-% creategame USERNAME:POSITION:GAMENAME:PLAYERLIMIT:TEAMLAYOUT
-% This creates a game in the system and creates the host player for the game.
-% POSITION: The player army faction position in the game (the player order)
-% GAMENAME: The title of this game
-% PLAYERLIMIT: The total amount of players that can be in this game
-% TEAMLAYOUT: The layout of the teams {"AB" would mean p1 is A team and p2 is B
-% team.}
-% joingame USERNAME:POSITION:GAMENAME
-% This allows a player to join into an already created game
-% addaction USERNAME:GAMENAME:P(0):P(1):...:P(n)
-% This allows to player to push actions into the action list. An empty list will
-% change to the next player turn.
-% P(n): A section of a string, int, or float array. Can extend to as many as
-% needed.
-% leavegame USERNAME:GAMENAME
-% In an inactive game, it'll remove the player from the list of players
-% In an active game, it'll change the player to inactive making them unable to
-% take any turns.
-
-
-%--------------------------------------------------------------------------------%
-
-
-% creategame USERNAME:POSITION:GAMENAME:PLAYERLIMIT:TEAMLAYOUT
+%%% CWT-Prolog Server
 
 
 :- module(database,
@@ -43,6 +8,7 @@
 
 :- use_module(library(persistency)).
 :- use_module(library(mavis)).
+:- use_module(library(lambda)).
 
 
 :- persistent
@@ -65,9 +31,8 @@ login(User, Response) :-
   ->
      login_response(user_exists, Response)
   ;
-     login_response(no_user, Response),
      add_user(User),
-     db_sync(reload)
+     login_response(no_user, Response)
   ).
 
 
@@ -81,9 +46,7 @@ logout(User, Response) :-
   (
      current_user(User)
   ->
-     db_sync(gc),
      remove_user(User),
-     db_sync(reload),
      logout_response(user_exists, Response)
   ;
      logout_response(no_user, Response)
@@ -106,16 +69,20 @@ current_user(User) :-
 %--------------------------------------------------------------------------------%
 
 
-%% add_user(+User:atom) is semidet.
+%% add_user(+User:string) is semidet.
 
 add_user(User) :-
-  with_mutex(user_db, assert_user(User)).
+  Add_sync =
+    (\User^(assert_user(User), db_sync(reload))),
+  with_mutex(user_db, call(Add_sync, User)).
 
 
 %% remove_user(+User:atom) is semidet.
 
 remove_user(User) :-
-  with_mutex(user_db, retract_user(User)).
+  Remove_sync =
+    (\User^(db_sync(gc), retract_user(User), db_sync(reload))),
+  with_mutex(user_db, call(Remove_sync, User)).
 
 
 %--------------------------------------------------------------------------------%
