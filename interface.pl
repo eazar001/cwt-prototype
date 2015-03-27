3b655bde82c968167651896ea78d0d0cf895c03e
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-03-26 20:11:13 -0700

    Reorganizing and documenting interface.  Changes to be committed: 	modified:   interface.pl

--- interface.pl
+++ interface.pl
@@ -12,16 +12,33 @@
 :- use_module(library(func)).
 :- use_module(database).
 
+
+%--------------------------------------------------------------------------------%
+% Main Interface
+%--------------------------------------------------------------------------------%
+
+
 :- dynamic port/1.
 
-:- http_handler(/, status, []).
+:- http_handler(/, server_status, []).
 :- http_handler('/login', login, []).
 :- http_handler('/logout', logout, []).
 
-main :-
-  attach_db('store.db'),
-  server(port_here).
 
+%% start_server(+File:atom, +Port:integer) is semidet.
+%
+% Attach to the specified database file, and start the server on the specified
+% port.
+
+start_server(File, Port) :-
+  between(1, 0xffff, Port),
+  attach_db(File),
+  server(Port).
+
+
+%% server(+Port:integer)
+%
+% Starts server on requested port. Asserts port at top level.
 
 server(Port) :-
   (  port(Port)
@@ -31,25 +48,57 @@ server(Port) :-
   http_server(http_dispatch, [port(Port)]).
 
 
-status(_Request) :-
+%% server_status is det.
+%
+% Friendly message to let client know that the server is up.
+
+server_status(_Request) :-
   format('Content-type: text/plain~n~n'),
   format('Server is up.~n').
 
-login(Query) :-
-  http_parameters(Query, [name(User, [string])]),
-  login(User, Status),
+
+%% send_status(+Status:string) is det.
+%
+% Takes a response status and sends the information to the client.
+
+send_status(Status) :-
   format('Content-type: text/plain~n~n'),
   format('~s', [Status]).
 
-logout(Query) :-
-  http_parameters(Query, [name(User, [string])]),
-  logout(User, Status),
-  format('Content-type: text/plain~n~n'),
-  format('~s', [Status]).
+
+%% disconnect is det.
+%
+% Shut down server on specified port and clean up information from top level.
 
 disconnect :-
   port(Port),
-  http_stop_server(Port, []).
+  http_stop_server(Port, []),
+  retractall(port(_)).
+
+
+%--------------------------------------------------------------------------------%
+% Queries
+%--------------------------------------------------------------------------------%
+
+
+%% login(+Query:compound) is det.
+%
+% Attempt login and send status back to client.
+
+login(Query) :-
+  http_parameters(Query, [name(User, [string])]),
+  send_status(login $ User).
+
+
+%% login(+Query:compound) is det.
+%
+% Attempt logout and send status back to client.
+
+logout(Query) :-
+  http_parameters(Query, [name(User, [string])]),
+  send_status(logout $ User).
+
+
 
 
 
